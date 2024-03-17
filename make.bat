ca78ee136eea4e23283c906019decc722eebb942
Yula1234 <vpervuhina0@gmail.com>
2024-03-17 17:23:35 +0500

    Enable all warnings

--- make.bat
+++ make.bat
@@ -1,3 +1,3 @@
 @echo off
 echo Compiling src/main.cpp...
-g++ -std=c++2a -fmax-errors=2 -Wextra -pedantic -Wall -Werror -Wswitch src/main.cpp -o bpm.exe -ggdb
+g++ -std=c++2a -fmax-errors=2 -Wdouble-promotion -Wdiv-by-zero -Wold-style-cast -Wextra -pedantic -Wall -Werror -Wswitch src/main.cpp -o bpm.exe -ggdb
